840578f86fbe0088cec45ac2580b47821e0c4597
JwowSquared <Jdwagner1@hotmail.com>
2021-01-22 21:33:41 +0000

    added task 5 script and template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""x"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def a():
+    """x"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def b():
+    """x"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """x"""
+    return "C {}".format(text).replace("_", " ")
+
+
+@app.route("/python", defaults={"text": "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def d(text):
+    """x"""
+    return "Python {}".format(text).replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def e(n):
+    """x"""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def f(n):
+    """x"""
+    return render_template("5-number.html", num=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
