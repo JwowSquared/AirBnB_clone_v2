4402eb02c68aa5421c4ceb7517115627e772aba0
JwowSquared <Jdwagner1@hotmail.com>
2021-01-22 21:57:04 +0000

    added engine type check that will maybe fix my problem

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -11,13 +12,14 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="states", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """getter for FileStorage use"""
-        # potentially need to encase this in an if statement to protect
-        from models.base_model import storage
-        out = []
-        for obj in storage.all(City).values():
-            if obj.state_id == self.id:
-                out.append(obj)
-        return out
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter for FileStorage use"""
+            # potentially need to encase this in an if statement to protect
+            from models.base_model import storage
+            out = []
+            for obj in storage.all(City).values():
+                if obj.state_id == self.id:
+                    out.append(obj)
+            return out
