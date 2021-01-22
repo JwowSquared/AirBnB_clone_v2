e8ab0388b698927c1e87bd26f01aecd39d9631ab
JwowSquared <Jdwagner1@hotmail.com>
2021-01-22 21:22:22 +0000

    added task 4 script that probably isnt all the way correct

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""x"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def a():
+    """x"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def b():
+    """x"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """x"""
+    return "C {}".format(text).replace("_", " ")
+
+
+@app.route("/python", defaults={"text": "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def d(text):
+    """x"""
+    return "Python {}".format(text).replace("_", " ")
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def e(n):
+    """x"""
+    if n.isdigit():
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
