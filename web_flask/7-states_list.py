d49bea6648b913126c46e1278ffd688c691dbe74
JwowSquared <Jdwagner1@hotmail.com>
2021-01-23 03:13:42 +0000

    added task 8 flask script and template

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""x"""
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def a():
+    """x"""
+    states = []
+    raw = storage.all("State").values()
+    for state in raw:
+        states.append({state.id: state.name})
+    return render_template("7-states_list.html", states=states)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
