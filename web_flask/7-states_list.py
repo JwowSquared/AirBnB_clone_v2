6793dbed551b280cdda99eb8b3645f8aeff0dcd7
JwowSquared <Jdwagner1@hotmail.com>
2021-01-23 03:15:49 +0000

    added storage.close

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,5 +15,11 @@ def a():
         states.append({state.id: state.name})
     return render_template("7-states_list.html", states=states)
 
+
+@app.teardown_appcontext
+def b():
+    """x"""
+    storage.close()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
